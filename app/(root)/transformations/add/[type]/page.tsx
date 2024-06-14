efb681a250465da9500c742eea4d8f688fa8d7fb
osama2000code <oss909@yahoo.com>
2024-06-14 19:14:28 +0300

    webhooks clerk-1

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,32 @@
+import TransformationForm from '@/components/shared/TransformationForm'
 import Header from '@/components/shared/header'
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-const AddTransformationsPage = () => {
-  return (
-        <div>
+
+const AddTransformationsPage = async ({params: { type }}:SearchParamProps) => {
+        const transformations =transformationTypes[type]
+        const {userId} =auth();
+        if (!userId) redirect('/sing-in')
+
+        const user = await getUserById(userId);
+    return (
+
+        <>
         <Header 
-        title='Transformations title'
-        subtitle='Transformations sub title'
+        title={transformations.title}
+        subtitle={transformations.subTitle}
         />
-        </div>
+
+        <TransformationForm 
+        action='Add'
+        userId={user._id}
+        type={transformations.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+        />
+        </>
     )
 }
 
