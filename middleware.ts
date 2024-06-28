501849c429fe4a8c3f4bca1e13eaed2244b4d5b1
osama2000code <oss909@yahoo.com>
2024-06-28 20:17:17 +0300

    webhooks clerk-2

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
+  import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-const isProtectedRoute= createRouteMatcher(['/api/webhooks/clerk']);
-export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  });
-export const config = {
-    // The following matcher runs middleware on all routes
-    // except static assets.
-    matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-  };
+const isProtectedRoute = createRouteMatcher([
+    '/api/webhooks/clerk'
+    
+    ]);
+    export default clerkMiddleware((auth, req) => {
+        if (isProtectedRoute(req)) auth().protect();
+    });
+    export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    };
