d5da5ca6526b1b635de7a12f95a1829ee3d37ffa
osama2000code <oss909@yahoo.com>
2024-07-04 22:07:55 +0300

    webhooks clerk-4

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,9 @@ return(
     <>
 <div className="h-auto bg-gradient-to-r from-cyan-500 to-blue-500">
         <div className="flex items-center justify-center h-screen">
-            <SignIn />
+            <SignIn 
+            afterSignOutUrl={'/'}
+            />
         </div>
 </div>
 
